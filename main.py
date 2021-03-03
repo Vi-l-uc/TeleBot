78df714cf0dc11ecde464fa41b96a58e9c1f9f5c
Vi-l-uc <viluc@bk.ru>
2021-03-04 00:42:32 +0300

    Exception

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 import telebot
 import requests
 import json
+
 TOKEN = '1509594923:AAFDUTQCO7aTxcBLOgHJ7mWiQBIJaKYXIBc'
+
 bot = telebot.TeleBot(TOKEN)
 
 keys = {"доллар": "USD",
         "евро": "EUR",
         "рубль": "RUB",
 }
+
+
+class ConvertionException(Exception):
+    pass
+
+class CriptoConvecter:
+    @staticmethod
+    def convector( quote: str, base: str, amount: str):
+
+
+
+        if quote == base:
+            raise ConvertionException(f"Невозможно перевести {base} в {base}")
+
+        try:
+            quote_tic = keys[quote]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {quote}")
+
+        try:
+            base_tic = keys[base]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {base}")
+
+        #try:
+        #    amount = float(amount)
+        #except ValueError:
+        #    raise ConvertionException(f'Не удалось обработать {amount}')
+
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_tic}&tsyms={base_tic}")
+        total = round(float(json.loads(r.content)[keys[base]])*float(amount), 2)
+
+        return total
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -30,11 +65,15 @@ def values(message: telebot.types.Message):
     bot.reply_to(message, text)
 
 
-@bot.message_handler(convent_types=['text'])
+@bot.message_handler(content_types=['text'])
 def values(message: telebot.types.Message):
-    quote, base, amount = message.text.split(' ')
-    r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}")
-    total = json.loads(r.content)[keys[base]]
+    val = message.text.split(' ')
+
+    if len(val) != 3:
+        raise ConvertionException("Лишние данные или недостаточно данных")
+
+    quote, base, amount = val
+    total = CriptoConvecter.convector(quote, base, amount)
     text = f'Цена {amount} {quote} в {base} -  {total}'
     bot.send_message(message.chat.id, text)
 
